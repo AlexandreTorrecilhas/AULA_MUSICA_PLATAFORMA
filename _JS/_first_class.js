060412dabb476db9c5c9c211547d6f48c923ed3e
AlexandreTorrecilhas <69021088+AlexandreTorrecilhas@users.noreply.github.com>
2022-07-16 18:20:08 -0300

    Implementation of Design Slides
    Here I fixed the css of second slide, and link the div with the radio input, then when I click inside the border, it activates the radio button

--- _JS/_first_class.js
+++ _JS/_first_class.js
@@ -9,24 +9,34 @@ function plusSlides(value){
         for(let contator = 0; contator < slides.length; contator++){
             slides[contator].style.display = "none";
         }
-        slides[result].style.display = 'flex';
-        slides[result].style.flexDirection = "column";
+        build_slide_style(result)
     }
     else if(result >= slides.length){
         result = slides.length - 1;
         for(let contator = 0; contator < slides.length; contator++){
             slides[contator].style.display = "none";
         }
-        slides[result].style.display = 'flex';
-        slides[result].style.flexDirection = "column";
+        build_slide_style(result)
     }
     else{
         for(let contator = 0; contator < slides.length; contator++){
             slides[contator].style.display = "none";
         }
-        slides[result].style.display = 'flex';
-        slides[result].style.flexDirection = "column";
+        build_slide_style(result)
     }
+}
+function build_slide_style(result){
+    let slides = document.getElementsByClassName('mySlides');
+    slides[result].style.display = "flex";
+    slides[result].style.flexDirection = "Column";
+    slides[result].style.alignItems = "center";
+    slides[result].style.justifyContent = "space-around";
+    slides[result].style.height = "60vh";
+    slides[result].style.border = "1px solid red";
+}
+function testFunction(value){
+    let answer = document.getElementsByClassName("question_answer");
+    answer[value].checked = true;
 }
 function seeResult(answer){
     let result = document.getElementsByName("firstquestion");
